d242d77dace6630d006f38f41feaa14b1258d8de
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-04-04 21:34:51 +0900

    Update Heatmap.R

--- Heatmap.R
+++ Heatmap.R
@@ -32,7 +32,7 @@ tw_time_jst <- tw_time_utc %>%
   as.POSIXlt(tz = "Japan")
 
 
-# 年月日別に作図 --------------------------------------------------------------------
+# 年・月・日別に作図 --------------------------------------------------------------------
 
 # セルの単位(期間)を指定
 #term <- "year"
@@ -99,65 +99,42 @@ if(term == "day") {
 }
 
 
-####時別####
+
+# 時別に作図 -------------------------------------------------------------------
+
 # 期間別ツイート数の集計
-ntweet_df1 <- data.frame(terms = as.POSIXct(floor_date(tw_time_jst, "hour")), 
-                         tweets = 1) %>% 
-  group_by(terms) %>% 
-  summarise(tweets = sum(tweets))
+ntweet_df1 <- tw_time_jst %>% 
+  floor_date(unit = "hour") %>% 
+  as.POSIXct() %>% 
+  tibble(terms = .) %>% 
+  count(terms)
 
 # 初ツイートから現在までの期間
-nterm <- seq(floor_date(tw_time_jst[length(tw_time_jst)], "hour"), 
-             floor_date(Sys.time(), "hour"), 
-             by = "hour")
-nterm_df <- data.frame(terms = nterm)
+nterm_df <- seq(
+  floor_date(tw_time_jst[length(tw_time_jst)], "hour"), 
+  floor_date(Sys.time(), "hour"), 
+  by = "hour"
+) %>% 
+  tibble(terms = .)
 
 # 全期間別のツイート数
 ntweet_df2 <- left_join(nterm_df, ntweet_df1, by = "terms")
-ntweet_df2$tweets[is.na(ntweet_df2$tweets)] <- 0
+ntweet_df2[["n"]][is.na(ntweet_df2[["n"]])] <- 0
 
 
 # データフレームの整形
-ntweet_df2$terms1 <- ntweet_df2$terms %>% 
-  str_remove_all(" \\d{2}:\\d{2}:\\d{2}$")
-
-ntweet_df2$terms2 <- ntweet_df2$terms %>% 
-  str_remove_all("^\\d{4}-\\d{2}-\\d{2} ") %>% 
-  str_remove_all(":\\d{2}:\\d{2}$")
-
-ntweet_df3 <- ntweet_df2 %>% 
-  select(terms1, terms2, tweets)
-
-# プロットする期間の指定
-day_1 <- "2019-01-01" # 開始日を指定
-day_1 <- floor_date(tw_time_jst[length(tw_time_jst)], term) # 一番古いツイート日を指定
-
-day_100 <- seq(as.Date(day_1), by = "day", len = 100) %>% 
-  as.character() %>% 
-  paste(collapse = "|")
-ntweet_df4 <- filter(ntweet_df3, grepl(day_100, terms1))
-
+ntweet_df2 <- ntweet_df2 %>% 
+  mutate(year_mon_day = as_date(terms)) %>% 
+  mutate(hour = str_remove_all(terms, "^\\d{4}-\\d{2}-\\d{2} |:\\d{2}:\\d{2}$"))
 
 # ヒートマップの作図
-ggplot(ntweet_df4, mapping = aes(x = terms1, y = terms2, fill = tweets)) + 
+ggplot(ntweet_df2, aes(x = year_mon_day, y = hour, fill = n)) + 
   geom_tile() + 
   scale_fill_gradient(low = "white" , high = "#00A968") + 
+  scale_x_date(breaks = seq(ntweet_df2[["year_mon_day"]][1], 
+                            ntweet_df2[["year_mon_day"]][nrow(ntweet_df2)], 
+                            by = "1 week")) + 
   theme(axis.text.x = element_text(angle = 90)) + 
-  labs(title = paste("「@", screen_name, "」のツイート数のヒートマップ", sep = ""), 
-       x = "", y = "")
-
-
-scale_x_discrete(breaks = seq(1, nrow(ntweet_df3), by = 3), 
-                 labels = as.character(seq(as.Date(ntweet_df3$terms1[1]), 
-                                           as.Date(ntweet_df3$terms1[nrow(ntweet_df3)]), 
-                                           by = "3 day")))
-
-
-
-####try####
-scale_x_discrete(breaks = seq(1, nrow(ntweet_df3), by = 3), 
-                 labels = as.character(seq(as.Date(ntweet_df3$terms1[1]), as.Date(ntweet_df3$terms1[nrow(ntweet_df3)]), by = "3 day"))) + 
-  
-  as.Date("2019-01", "%Y-%m")
-as.Date(floor_date(tw_time_jst, term), format = "%Y-%m-%d")
+  labs(title = paste0("@", screen_name, "のツイート数"), 
+       x = "year-mon-day", y = "hour")
 
