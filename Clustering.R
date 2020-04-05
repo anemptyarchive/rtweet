92ee894334cfa6d0ce3d4fb25c6eb13090bb7b74
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-04-05 17:51:25 +0900

    ver1.0

--- Clustering.R
+++ Clustering.R
@@ -1,19 +1,15 @@
 
 # ツイート頻度でクラスタリング---------------------------------------------------------------
 
-## 利用パッケージ
-# ツイート収集
-library(rtweet)    # get_timeline()
-# データフレーム操作
-library(dplyr)
-library(tidyr)     # gather()
-# 文字列操作
-library(stringr)   # str_remove_all()
-# 時間データ操作
-library(lubridate) # floor_date()
-# 作図
-library(ggplot2)
-library(ggdendro)  # ggdendrogram()
+
+# 利用パッケージ -----------------------------------------------------------------
+
+library(rtweet) # ツイート収集:get_timeline()
+library(dplyr) # データフレーム操作
+library(lubridate) # 時間データ操作:floor_date()
+library(tidyr) # データフレーム操作:pivot_longer()
+library(ggplot2) # 作図
+library(ggdendro) # 作図:ggdendrogram()
 
 
 # ツイート収集 ------------------------------------------------------------------
@@ -26,7 +22,6 @@ screen_names <- c(
   "GAMBA_OFFICIAL", "crz_official", "visselkobe", "sanfrecce_SFC", 
   "saganofficial17", "TRINITAofficial"
 )
-
 
 # 期間を指定
 tw_count <- seq(
@@ -50,7 +45,8 @@ for(i in seq_along(screen_names)) {
   tmp_tw_count <- tw_time %>% 
     floor_date(unit = "hour") %>% # 指定した単位に切り捨て
     tibble(terms = .) %>% # データフレームに変換
-    count(terms) # ツイート数をカウント
+    group_by(terms) %>% # グループ化
+    summarise(!!screen_names[i] := n()) # ツイート数をカウント
   
   # 集計結果を結合
   tw_count <- left_join(tw_count, tmp_tw_count, by = "terms")
@@ -59,118 +55,47 @@ for(i in seq_along(screen_names)) {
   Sys.sleep(1)
 }
 
-## データフレームを整形
-# 列名を付ける
-colnames(tw_count) <- c("time", paste0("@", screen_names))
-
-# NAを0に置換
+# ツイートがないと値がNAとなるので0に置換
 tw_count[is.na.data.frame(tw_count)] <- 0
 
 # データフレームをlong型に変換
 tw_count_long <- pivot_longer(
   tw_count, 
-  cols = -time, 
-  names_to = "screen_name", 
-  values_to = "n"
+  cols = -terms, # 変換しない列
+  names_to = "screen_name", # 現列名を格納する列の名前
+  values_to = "n" # 現セルを格納する列の名前
 )
 
 # ヒートマップを作図
-ggplot(tw_count_long, aes(x = screen_name, y = time, fill = n)) +  # データ
+ggplot(tw_count_long, aes(x = screen_name, y = terms, fill = n)) +  # データ
   geom_tile() + 
   scale_fill_gradient(low = "white", high = "#00A968") +  # 塗りつぶし色のグラデーション
-  scale_y_datetime(date_breaks = "1 day", date_labels = "%Y-%m-%d %H") +  # y軸目盛
+  scale_y_datetime(date_breaks = "1 day", 
+                   date_labels = "%Y-%m-%d %H") +  # y軸目盛(日時)
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + # x軸目盛の角度
   labs(title = "アカウントごとのツイート数")
 
 
 # クラスタリング -----------------------------------------------------------------
 
-
-# データフレームを整形
-cluster <- tw_count[, -1] # 非数値の列を落とす
-
-# 類似度を測る
-den <- cluster %>% 
-  t() %>%     # 転置
-  dist() %>%  # 
-  hclust("ward.D2") %>% # 
-  dendro_data()         # 作図用にデータを変換
-
-# 作図
-ggdendrogram(den, theme_dendro = FALSE)
-
-
-# 類似度を測る
-den <- cluster %>% 
-  t() %>% 
-  dist() %>% 
-  hclust("ward.D2") %>% 
-  as.dendrogram()
+# クラスタリング
+res_dendrogram <- tw_count[, -1] %>% # 非数値の列を落とす
+  t() %>% # 転置
+  dist() %>% # 距離(類似度)を測る
+  hclust("ward.D2") %>% # クラスタリング
+  dendro_data() # 作図用にデータを変換
 
 # 描画
-plot(den)
+ggdendrogram(res_dendrogram, theme_dendro = FALSE)
 
 
+# クラスタリング
+res_dendrogram <- tw_count[, -1] %>% # 非数値の列を落とす
+  t() %>% # 転置
+  dist() %>% # 距離(類似度)を測る
+  hclust("ward.D2") %>% # クラスタリング
+  as.dendrogram() # 作図用にデータを変換
 
+# 描画
+plot(res_dendrogram)
 
-# try ---------------------------------------------------------------------
-
-# 期間を指定
-term_from <- as.POSIXct("2019/08/21", tz = "Japan")
-term_to   <- as.POSIXct("2019/08/27", tz = "Japan")
-term_vec <- seq(term_from, term_to, by = "hour")
-
-
-# 追加パッケージ
-library(scales) # date_breaks(), date_format()
-
-# 作図
-ggplot(data = count_df_long, mapping = aes(x = account, y = time, fill = n)) +  # データ
-  geom_tile() + 
-  scale_fill_gradient(low = "#FFFFFF", high = "#00A968") +    # 塗りつぶし色のグラデーション
-  scale_y_datetime(breaks = date_breaks("3 hour"),            # y軸目盛：表示位置
-                   labels = date_format("%Y-%m-%d %H:%M", tz = "Japan")) +  # y軸目盛：表示テキスト
-  theme(axis.text.x = element_text(angle = 90))               # x軸目盛の角度
-
-apply(count_df[, -1], 2, sum)
-class(count_df_long$time)
-
-
-# 利用パッケージ
-library(ggdendro)
-
-# 類似度を測る
-den <- cluster %>% 
-       t() %>%     # 転置
-       dist() %>%  # 
-       hclust("ward.D2") %>% # 
-       dendro_data()         # 作図用にデータを変換
-den2 <- dendro_data(den)
-
-# 作図
-ggdendrogram(den, theme_dendro = FALSE)
-
-
-# 作図：ggplot()
-ggplot() + 
-  geom_segment(data = segment(den), 
-               aes(x = x, y = y, xend = xend, yend = yend)) + 
-  geom_text(data = label(den), 
-            aes(x = x, y = y, label = label), hjust = 1, angle = 90) + 
-  ylim(c(-10, 50))
-
-
-
-# ツイートを保存しておく -------------------------------------------------------------
-
-for(i in seq_along(screen_name)) {
-  
-  # ツイートを収集
-  tw_data <- get_timeline(screen_name[i], n = 10000, include_rts = FALSE)
-  
-  # 保存
-  saveRDS(tw_data, paste0("tw_data_Jleague/", screen_name[i], ".obj"))
-  
-  # おまじない
-  Sys.sleep(1)
-}
