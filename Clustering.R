09415792e81dbf590bfe5e7e5cf03e527dc97b8e
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-04-05 17:07:03 +0900

    Update Clustering.R

--- Clustering.R
+++ Clustering.R
@@ -19,7 +19,7 @@ library(ggdendro)  # ggdendrogram()
 # ツイート収集 ------------------------------------------------------------------
 
 # アカウントを指定
-screen_name <- c(
+screen_names <- c(
   "consaofficial", "vega_official_", "atlrs_official", "REDSOFFICIAL", 
   "fctokyoofficial", "frontale_staff", "prompt_fmarinos", "bellmare_staff", 
   "yamagafc", "spulse_official", "Jubiloiwata_YFC", "nge_official", 
@@ -29,65 +29,72 @@ screen_name <- c(
 
 
 # 期間を指定
-term_vec <- seq(as.POSIXct("2019/08/21", tz = "Japan"), len = 24 * 7, by = "hour")
-count_df <- data.frame(TERM = term_vec)
+tw_count <- seq(
+  as.POSIXct("2020/01/01", tz = "Japan"), # から
+  as.POSIXct("2020/02/01", tz = "Japan"), # まで
+  by = "hour"
+) %>% 
+  tibble(terms = .)
 
 # ツイート収集と集計
-for(i in seq_along(screen_name_vec)) {
+for(i in seq_along(screen_names)) {
   
   # ツイートを収集
-  tmp_tw_data <- get_timeline(screen_name_vec[i], n = 1000, include_rts = FALSE)
+  tw_data <- get_timeline(screen_names[i], n = 10000, include_rts = FALSE)
 
-  # ツイート日時を抽出
-  tmp_tw_time <- tw_data$created_at %>% 
-                 as.POSIXlt(tz = "Japan") # タイムゾーンを変換
+  # ツイート日時データを変換
+  tw_time <- tw_data[["created_at"]] %>% # ツイート日時を抽出
+    as.POSIXct(tz = "Asia/Tokyo") # 日本標準時に変換
   
-  # 期間別ツイート数を集計
-  tmp_count_df <- data.frame(TERM = floor_date(tmp_tw_time, "hour")) %>% # データフレームを作成
-                  group_by(TERM) %>%     # 期間ごとにグループ化
-                  summarise(COUNT = n()) # グループごとにカウント
+  # 指定した期間ごとにツイート数を集計
+  tmp_tw_count <- tw_time %>% 
+    floor_date(unit = "hour") %>% # 指定した単位に切り捨て
+    tibble(terms = .) %>% # データフレームに変換
+    count(terms) # ツイート数をカウント
   
   # 集計結果を結合
-  count_df <- left_join(count_df, tmp_count_df, by = "TERM")
+  tw_count <- left_join(tw_count, tmp_tw_count, by = "terms")
   
   # おまじない
   Sys.sleep(1)
 }
-?floor_date
+
 ## データフレームを整形
 # 列名を付ける
-colnames(count_df) <- c("time", paste0("@", screen_name_vec))
+colnames(tw_count) <- c("time", paste0("@", screen_names))
 
 # NAを0に置換
-count_df[is.na.data.frame(count_df)] <- 0
+tw_count[is.na.data.frame(tw_count)] <- 0
 
 # データフレームをlong型に変換
-count_df_long <- gather(count_df, key = "account", value = "n", -time)
+tw_count_long <- pivot_longer(
+  tw_count, 
+  cols = -time, 
+  names_to = "screen_name", 
+  values_to = "n"
+)
 
-# y軸ラベル用のベクトルを作成
-y_label <- term_vec[seq(1, length(term_vec), by = 6)] # 間引く間隔を指定
-
-# 作図
-ggplot(data = count_df_long, mapping = aes(x = account, y = time, fill = n)) +  # データ
+# ヒートマップを作図
+ggplot(tw_count_long, aes(x = screen_name, y = time, fill = n)) +  # データ
   geom_tile() + 
-  scale_fill_gradient(low = "#FFFFFF", high = "#00A968") +  # 塗りつぶし色のグラデーション
-  scale_y_datetime(breaks = y_label, 
-                   labels = str_remove(y_label, ":00$")) +  # y軸目盛
-  theme(axis.text.x = element_text(angle = 90))             # x軸目盛の角度
-  
+  scale_fill_gradient(low = "white", high = "#00A968") +  # 塗りつぶし色のグラデーション
+  scale_y_datetime(date_breaks = "1 day", date_labels = "%Y-%m-%d %H") +  # y軸目盛
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + # x軸目盛の角度
+  labs(title = "アカウントごとのツイート数")
+
 
 # クラスタリング -----------------------------------------------------------------
 
 
 # データフレームを整形
-cluster <- count_df[, -1] # 非数値の列を落とす
+cluster <- tw_count[, -1] # 非数値の列を落とす
 
 # 類似度を測る
 den <- cluster %>% 
-       t() %>%     # 転置
-       dist() %>%  # 
-       hclust("ward.D2") %>% # 
-       dendro_data()         # 作図用にデータを変換
+  t() %>%     # 転置
+  dist() %>%  # 
+  hclust("ward.D2") %>% # 
+  dendro_data()         # 作図用にデータを変換
 
 # 作図
 ggdendrogram(den, theme_dendro = FALSE)
