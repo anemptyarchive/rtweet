a378fb99a623e8d09620902527c3024bfc0beb85
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2022-06-09 18:52:32 +0900

    Update Heatmap.R

--- Code/Heatmap.R
+++ Code/Heatmap.R
@@ -1,106 +1,118 @@
 
 # ツイート数をヒートマップで可視化 --------------------------------------------------
 
+# 利用パッケージ
+library(rtweet)
+library(tidyverse)
+library(lubridate)
 
-# 利用パッケージ -----------------------------------------------------------------
-
-library(rtweet) # ツイート収集:get_timeline(), search_tweets()
-library(dplyr) # データフレーム操作
-library(tidyr) # データフレーム操作:replace_na()
-library(lubridate) # 時間データ操作:floor_date(), as_date(), now()
+# チェック用
 library(ggplot2) # 作図
 
 
-# ツイート収集 ------------------------------------------------------------------
+# ツイートの収集 ------------------------------------------------------------------
 
 # アカウントを指定
 screen_name <- "anemptyarchive"
 
-# アカウント指定でツイートを収集
-tw_data <- get_timeline(screen_name, n = 10000, include_rts = FALSE)
+# アカウントでツイートを収集
+tw_df <- rtweet::get_timeline(screen_name, n = 10000, include_rts = TRUE)
 
-# キーワードを指定
+
+# 単語を指定
 #search_word <- "トピックモデル"
-# 単語指定でツイートを収集
-#tw_data <- search_tweets(search_word, n = 30000, include_rts = FALSE)
 
-# ツイート日時データを変換
-tw_time <- tw_data[["created_at"]] %>% # ツイート日時を抽出
-  as.POSIXct(tz = "Etc/GMT") %>% # POSIXct型の協定世界時を明示
-  as.POSIXlt(tz = "Asia/Tokyo") # POSIXlt型の日本標準時に変換
+# 単語でツイートを収集
+#tw_df <- rtweet::search_tweets(search_word, n = 30000, include_rts = FALSE)
 
 
-# 期間(年・月・日)を指定してカウント --------------------------------------------------------------------
+# 期間の指定 -------------------------------------------------------------------
 
-# セルの単位(区切る期間)を指定
-#term <- "year"
-#term <- "mon"
+# 集計開始日を指定
+date_from <- "2021-01-01"
+date_from <- min(tw_df[["created_at"]])
+
+# 集計終了日を指定
+date_to <- "2021-12-31"
+date_to <- max(tw_df[["created_at"]])
+date_to <- lubridate::today()
+
+
+# 期間(年・月・日)を指定して集計 ----------------------------------------------------------------------
+
+# 集計単位を指定
+term <- "year"
+term <- "mon"
 term <- "day"
 
-# 指定した期間ごとにツイート数を集計
-tw_count1 <- tw_time %>% 
-  floor_date(unit = term) %>% # 指定した期間に切り捨て
-  as_date() %>% # Date型に変換
-  tibble(terms = .) %>% # データフレームに変換
-  count(terms) # ツイート数をカウント
+# Date型に変換
+date_from <- date_from |> 
+  lubridate::as_date() |> 
+  lubridate::floor_date(unit = term)
+date_to <- date_to |> 
+  lubridate::as_date() |> 
+  lubridate::floor_date(unit = term)
 
-# ツイートがない期間が欠落する対策
-term_df <- seq(
-  floor_date(tail(tw_time, 1), term), # 一番古い日時
-  floor_date(now(), term), # 現在日時
-  by = term
-) %>% # 指定した期間刻みのベクトルを作成
-  as_date() %>% # Date型に変換
-  tibble(terms = .) # データフレームに変換
+# 指定した単位ごとにツイート数を集計
+freq_df <- tw_df |> 
+  dplyr::select(date = created_at) |> # ツイート日時列を選択
+  dplyr::mutate(
+    date = date |> 
+      #lubridate::with_tz(tzone = "Etc/GMT") |> # POSIXct型の協定世界時を明示
+      lubridate::with_tz(tzone = "Asia/Tokyo") |> # POSIXct型の日本標準時に変換
+      lubridate::floor_date(unit = term) |> # 指定した単位に切り捨て
+      lubridate::as_date() # Date型に変換
+  ) |> # 
+  dplyr::count(date, name = "n") |> # ツイート数をカウント
+  dplyr::right_join(
+    tibble::tibble(date = seq(from = date_from, to = date_to, by = term)), 
+    by = "date"
+  ) |> # 全ての日付に統合
+  dplyr::mutate(n = tidyr::replace_na(n, replace = 0)) |> # ツイートの無い日付を0に置換
+  dplyr::arrange(date) # 昇順に並べ替え
 
-# 集計結果を結合
-tw_count2 <- left_join(term_df, tw_count1, by = "terms") %>% 
-  mutate(n = replace_na(n, 0)) # NAを0に置換
-
-# 軸ラベル用にデータフレームを整形
+# 指定した単位に応じて作図
 if(term == "day") {
+  # 軸用の文字列を作成
+  freq_df <- freq_df |> 
+    dplyr::mutate(
+      year_mon = format(date, format = "%Y-%m"), # 年月を抽出
+      day = format(date, format = "%d") # 日を抽出
+    )
   
-  tw_count2 <- tw_count2 %>% 
-    mutate(year_mon = format(terms, "%Y-%m")) %>% # 年月を抽出
-    mutate(day = format(terms, "%d")) # 日を抽出
-
-} else if(term == "mon") {
-  
-  tw_count2 <- tw_count2 %>% 
-    mutate(year = format(terms, "%Y")) %>% # 年を抽出
-    mutate(mon = format(terms, "%m")) # 月を抽出
-  
-} else if(term == "year") {
-  
-  tw_count2 <- tw_count2 %>% 
-    mutate(year = format(terms, "%Y")) # 年を抽出
-  
-}
-
-# ヒートマップを作図
-if(term == "day") {
-  
-  ggplot(tw_count2, aes(x = year_mon, y = day, fill = n)) + 
+  # ヒートマップを作成
+  ggplot(freq_df, aes(x = year_mon, y = day, fill = n)) + 
     geom_tile() + # ヒートマップ
-    scale_fill_gradient(low = "white" , high = "#00A968") + # 塗りつぶしの濃淡
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)) + # 軸目盛の傾き
+    scale_fill_gradient(low = "white" , high = "#00A968") + # 塗りつぶし色
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + # 軸目盛の傾き
     labs(title = paste0("@", screen_name, "のツイート数"), 
          x = "year-mon", y = "day") # ラベル
   
 } else if(term == "mon") {
+  # 軸用の文字列を作成
+  freq_df <- freq_df |> 
+    dplyr::mutate(
+      year = format(date, format = "%Y"), # 年を抽出
+      mon = format(date, "%m") # 月を抽出
+    )
   
-  ggplot(tw_count2, aes(x = year, y = mon, fill = n)) + 
+  # ヒートマップを作成
+  ggplot(freq_df, aes(x = year, y = mon, fill = n)) + 
     geom_tile() + # ヒートマップ
-    scale_fill_gradient(low = "white" , high = "#00A968") + # 塗りつぶしの濃淡
+    scale_fill_gradient(low = "white" , high = "#00A968") + # 塗りつぶし色
     labs(title = paste0("@", screen_name, "のツイート数"), 
          x = "year", y = "mon") # ラベル
   
 } else if(term == "year") {
+  # 軸用のラベルを作成
+  freq_df <- freq_df |> 
+    dplyr::mutate(year = format(date, format = "%Y")) # 年を抽出
   
-  ggplot(tw_count2, aes(x = year, y = 0, fill = n)) + 
+  # ヒートマップを作成
+  ggplot(freq_df, aes(x = year, y = 0, fill = n)) + 
     geom_tile() + # ヒートマップ
-    scale_fill_gradient(low = "white" , high = "#00A968") + # 塗りつぶしの濃淡
-    ylim(c(-1, 1)) + # y軸の範囲
+    scale_fill_gradient(low = "white" , high = "#00A968") + # 塗りつぶし色
+    scale_y_continuous(breaks = NULL, limits = c(-1, 1)) + # y軸目盛
     labs(title = paste0("@", screen_name, "のツイート数"), 
          x = "year", y = "") # ラベル
   
@@ -109,35 +121,44 @@ if(term == "day") {
 
 # 時別にカウント -------------------------------------------------------------------
 
-# 1時間ごとにツイート数を集計
-tw_count1 <- tw_time %>% 
-  floor_date(unit = "hour") %>% # 1時間単位で切り捨て
-  as.POSIXct() %>% # POSIXct型に変換
-  tibble(terms = .) %>% # データフレームに変換
-  count(terms) # ツイート数をカウント
+# POSIXct型に変換
+datetime_from <- date_from |> 
+  lubridate::as_date() |> 
+  lubridate::as_datetime(tz = "Asia/Tokyo")
+datetime_to <- date_to |> 
+  lubridate::as_date() |> 
+  lubridate::as_datetime(tz = "Asia/Tokyo") + lubridate::days(1)
 
-# ツイートがない期間が欠落する対策
-term_df <- seq(
-  floor_date(tw_time[length(tw_time)], "hour"), # 一番古い時刻
-  floor_date(now(), "hour"), # 現在時刻
-  by = "hour"
-) %>% # 1時間刻みのベクトルを作成
-  tibble(terms = .) # データフレームに変換
+# 時刻ごとにツイート数を集計
+freq_df <- tw_df |> 
+  dplyr::select(datetime = created_at) |> # ツイート日時列を選択
+  dplyr::mutate(
+    datetime = datetime |> 
+      #lubridate::with_tz(tzone = "Etc/GMT") |> # POSIXct型の協定世界時を明示
+      lubridate::with_tz(tzone = "Asia/Tokyo") |> # POSIXct型の日本標準時に変換
+      lubridate::floor_date(unit = "hour") # 1時間単位に切り捨て
+  ) |> # 
+  dplyr::count(datetime, name = "n") |> # ツイート数をカウント
+  dplyr::right_join(
+    tibble::tibble(
+      datetime = seq(from = datetime_from, to = datetime_to, by = "hour") |> 
+        head(n = -1)
+    ), 
+    by = "datetime"
+  ) |> # 全ての日時に統合
+  dplyr::mutate(
+    n = tidyr::replace_na(n, replace = 0), 
+    year_mon_day = lubridate::as_date(datetime), 
+    hour = format(datetime, format = "%H")
+  ) |> # ツイートの無い日時を0に置換
+  dplyr::arrange(datetime) # 昇順に並べ替え
 
-# 作図用にデータフレームを加工
-tw_count2 <- left_join(term_df, tw_count1, by = "terms") %>% # 集計結果を結合
-  mutate(n = replace_na(n, 0)) %>%  # NAを0に置換
-  mutate(year_mon_day = as_date(terms)) %>% # 年月日を抽出
-  mutate(hour = format(terms, "%H")) # 時間を抽出
-
-# ヒートマップを作図
-ggplot(tw_count2, aes(x = year_mon_day, y = hour, fill = n)) + 
+# ヒートマップを作成
+ggplot(freq_df, aes(x = year_mon_day, y = hour, fill = n)) + 
   geom_tile() + # ヒートマップ
-  scale_fill_gradient(low = "white" , high = "#00A968") + # 塗りつぶしの濃淡
-  scale_x_date(breaks = seq(tw_count2[["year_mon_day"]][1], 
-                            tw_count2[["year_mon_day"]][nrow(tw_count2)], 
-                            by = "1 week")) + # x軸目盛(日付)
-  theme(axis.text.x = element_text(angle = 90)) + # x軸目盛の傾き
+  scale_fill_gradient(low = "white" , high = "#00A968") + # 塗りつぶし色
+  scale_x_date(breaks = seq(lubridate::as_date(datetime_from), lubridate::as_date(datetime_to), by = "1 week")) + # x軸目盛
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + # x軸目盛の傾き
   labs(title = paste0("@", screen_name, "のツイート数"), 
        x = "year-mon-day", y = "hour") # ラベル
 
